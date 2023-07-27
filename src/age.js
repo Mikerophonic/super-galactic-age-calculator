b2888f08be0758e944ef58d8e5c98437b65abd35
Mikerophonic <micgcon@gmail.com>
2023-07-27 09:15:57 -0700

    add header, input, and results elements to html for UI

--- src/age.js
+++ src/age.js
@@ -6,31 +6,31 @@ export class Age {
         this.venusAge = "";
         this.marsAge = "";
         this.jupiterAge = "";
-        this.yearsFrom = {}
+        this.yearsFrom = {};
     }
 
     convertToPlanetAges() {
-        this.mercuryAge = Math.floor(this.earthAge / .24)
-        this.venusAge = Math.floor(this.earthAge / .62)
-        this.marsAge = Math.floor(this.earthAge / 1.88)
-        this.jupiterAge = Math.floor(this.earthAge / 11.86)
+        this.mercuryAge = Math.floor(this.earthAge / .24);
+        this.venusAge = Math.floor(this.earthAge / .62);
+        this.marsAge = Math.floor(this.earthAge / 1.88);
+        this.jupiterAge = Math.floor(this.earthAge / 11.86);
     }
 
     findYearsFrom() {
         if (this.earthAge > this.secondAge) {
-            let earthYears = this.earthAge - this.secondAge
-            this.yearsFrom.earthYears = earthYears
-            this.yearsFrom.mercuryYears = Math.floor(earthYears / .24)
-            this.yearsFrom.venusYears = Math.floor(earthYears / .62)
-            this.yearsFrom.marsYears = Math.floor(earthYears / 1.88)
-            this.yearsFrom.jupiterYears = Math.floor(earthYears / 11.86)
+            let earthYears = this.earthAge - this.secondAge;
+            this.yearsFrom.earthYears = earthYears;
+            this.yearsFrom.mercuryYears = Math.floor(earthYears / .24);
+            this.yearsFrom.venusYears = Math.floor(earthYears / .62);
+            this.yearsFrom.marsYears = Math.floor(earthYears / 1.88);
+            this.yearsFrom.jupiterYears = Math.floor(earthYears / 11.86);
         } else {
-            let earthYears = this.secondAge - this.earthAge
-            this.yearsFrom.earthYears = earthYears
-            this.yearsFrom.mercuryYears = Math.floor(earthYears / .24)
-            this.yearsFrom.venusYears = Math.floor(earthYears / .62)
-            this.yearsFrom.marsYears = Math.floor(earthYears / 1.88)
-            this.yearsFrom.jupiterYears = Math.floor(earthYears / 11.86)
+            let earthYears = this.secondAge - this.earthAge;
+            this.yearsFrom.earthYears = earthYears;
+            this.yearsFrom.mercuryYears = Math.floor(earthYears / .24);
+            this.yearsFrom.venusYears = Math.floor(earthYears / .62);
+            this.yearsFrom.marsYears = Math.floor(earthYears / 1.88);
+            this.yearsFrom.jupiterYears = Math.floor(earthYears / 11.86);
         }
     }
 }
